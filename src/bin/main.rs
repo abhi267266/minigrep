3b4f28cb523a1089e80c9e644665d89d00042de6
abhi267266 <abhi267266@gmail.com>
2025-03-26 19:46:30 +0530

    completed all the basic functionality

---
+++ src/bin/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use minigrep::Config;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    
+
+    //The bodies of the if let and the unwrap_or_else functions are the same in both cases: we print the error and exit.
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
+
