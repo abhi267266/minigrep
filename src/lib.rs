7f7156519b2df16b12aeb8698ce35f43878b371e
abhi267266 <abhi267266@gmail.com>
2025-03-31 12:00:33 +0530

    improved the search fn using iter and filter method in place of loop

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ pub struct Config {
 }
 
 use std::env;
-// --snip--
 
 impl Config {
     pub fn build(
@@ -57,28 +56,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
     }
-
-    results
-}
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 
